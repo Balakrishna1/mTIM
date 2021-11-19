ec20832fd97f7f037facb422a45c0956f52551ec
Balakrishna <balakrishna.gogineni@spacewell.com>
2021-11-19 17:10:48 +0530

    Added datatype enum.

---
+++ mTIM/mTIM/Enums/DataType.cs
@@ -0,0 +1,19 @@
+﻿namespace mTIM.Enums
+{
+    //ENUM(TaskValueType, None, Bool, String, Int, Float, ProjectData, Action, Reference, Doc, Count);
+    public enum DataType
+    {
+        None,
+        Bool,
+        String,
+        Int,
+        Float,
+        Prjladen,
+        Prjladen2,
+        Aktion,
+        Aktion2,
+        Referenz,
+        Doc,
+        Count
+    }
+}
